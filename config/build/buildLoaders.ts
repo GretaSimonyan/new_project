501d76cf866d9fcfab88ceb1b94867067897f849
GretaSimonyan <gsimonyan228@gmail.com>
2023-03-04 22:04:38 +0400

    5 Set up css modules

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -1,17 +1,27 @@
 import webpack from "webpack";
+import MiniCssExtractPlugin from "mini-css-extract-plugin";
+import {BuildOptions} from "./types/config";
 
-export function buildLoaders(): webpack.RuleSetRule[] {
+export function buildLoaders(options: BuildOptions): webpack.RuleSetRule[] {
+	const {
+		isDev,
+	} = options;
 
 	const cssLoader = {
 		test: /\.s[ac]ss$/i,
 		use: [
-			// Creates `style` nodes from JS strings
-			"style-loader",
-			// Translates CSS into CommonJS
-			"css-loader",
-			// Compiles Sass to CSS
-			"sass-loader",
-		],
+			isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+			{
+				loader: 'css-loader',
+				options: {
+					modules: {
+						auto: (resPath: string) => Boolean(resPath.includes('.module.')),
+						localIdentName: isDev ? '[path][name]__[local]' : '[hash:base64:8]'
+					},
+				}
+			},
+			'sass-loader',
+		]
 	}
 
 	const typescriptLoader = {
