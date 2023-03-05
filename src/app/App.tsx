682d458aa551585822618e888a6eed798c96bc9e
GretaSimonyan <gsimonyan228@gmail.com>
2023-03-05 18:58:48 +0400

    12 Navbar. Templates for development. First UI Kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,21 +1,18 @@
 import React from 'react';
 import './styles/index.scss';
-import {
-	Link,
-} from 'react-router-dom';
-import {classNames} from "shared/lib/classNames/classNames";
 import {useTheme} from "./providers/ThemeProvider";
+import {classNames} from "shared/lib/classNames/classNames";
 import AppRouter from "app/providers/router";
+import {Navbar} from "widgets/Navbar";
 
 const App = () => {
 	const {theme, toggleTheme} = useTheme();
 
 	return (
 		<div className={classNames('app', {}, [theme])}>
-			<button onClick={toggleTheme}>Theme</button>
-			<Link to={'/'}>Главная</Link>
-			<Link to={'/about'}>О сайте</Link>
+			<Navbar />
 			<AppRouter />
+			<button onClick={toggleTheme}>TOGGLE</button>
 		</div>
 	);
 };
