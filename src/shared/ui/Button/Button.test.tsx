a90cf301122c80e28fa55dd38f753000d1f67e04
GretaSimonyan <gsimonyan228@gmail.com>
2023-05-19 16:03:48 +0400

    24 React Testing Library. Component tests

---
+++ src/shared/ui/Button/Button.test.tsx
@@ -0,0 +1,19 @@
+import {
+  render, screen,
+} from '@testing-library/react';
+
+import {
+  Button, ThemeButton,
+} from './Button';
+
+describe('button', () => {
+  test('test render', () => {
+    render(<Button>test</Button>);
+    expect(screen.getByText('test')).toBeInTheDocument();
+  });
+  test('test clear theme', () => {
+    render(<Button theme={ThemeButton.CLEAR}>test</Button>);
+    expect(screen.getByText('test')).toHaveClass('clear');
+    screen.debug();
+  });
+});
