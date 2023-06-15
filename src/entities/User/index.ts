5a87450f019d3a96b2b8f28fbc96337351c6a74b
GretaSimonyan <gsimonyan228@gmail.com>
2023-06-15 14:46:55 +0400

    47 Router v6 private protected routes. Protected Routes

--- src/entities/User/index.ts
+++ src/entities/User/index.ts
@@ -5,3 +5,4 @@ export {
   userReducer, userActions,
 } from './model/slice/userSlice';
 export { getUserAuthData } from './model/selectors/getUserAuthData/getUserAuthData';
+export { getUserMounted } from './model/selectors/getUserMounted/getUserMounted';
