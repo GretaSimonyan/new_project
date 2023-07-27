476823dec022bb38c3178c775c8646996eb77864
GretaSimonyan <gsimonyan228@gmail.com>
2023-07-27 23:50:27 +0400

    82 Dynamic import. Lazy libraries. Provider. Gesture, React spring

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -16,6 +16,7 @@ import { Icon } from 'shared/ui/Icon/Icon';
 import NotificationIcon from 'shared/assets/icons/notification-20-20.svg';
 import { Popover } from 'shared/ui/Popups';
 import { Drawer } from 'shared/ui/Drawer/Drawer';
+import { AnimationProvider } from 'shared/lib/components/AnimationProvider';
 
 import { NotificationList } from '../../../../entities/Notifications';
 
@@ -56,9 +57,11 @@ export const NotificationButton = memo((props: NotificationButtonProps) => {
       </BrowserView>
       <MobileView>
         {trigger}
-        <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-          <NotificationList />
-        </Drawer>
+        <AnimationProvider>
+          <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+            <NotificationList />
+          </Drawer>
+        </AnimationProvider>
       </MobileView>
     </div>
   );
