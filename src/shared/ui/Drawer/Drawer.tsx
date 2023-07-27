090e68fa4f7a8a90073066d7c0334223d37ff0de
GretaSimonyan <gsimonyan228@gmail.com>
2023-07-27 23:03:13 +0400

    81 useModal. Refactoring Modal and Drawer

--- src/shared/ui/Drawer/Drawer.tsx
+++ src/shared/ui/Drawer/Drawer.tsx
@@ -5,6 +5,7 @@ import React, {
   memo, ReactNode,
 } from 'react';
 import { useTheme } from 'app/providers/ThemeProvider';
+import { useModal } from 'shared/lib/hooks/useModal/useModal';
 
 import { Overlay } from '../Overlay/Overlay';
 import { Portal } from '../Portal/Portal';
@@ -16,6 +17,7 @@ interface DrawerProps {
   children: ReactNode;
   isOpen?: boolean;
   onClose?: () => void;
+  lazy?: boolean;
 }
 
 export const Drawer = memo((props: DrawerProps) => {
@@ -24,20 +26,34 @@ export const Drawer = memo((props: DrawerProps) => {
     children,
     onClose,
     isOpen,
+    lazy,
   } = props;
   const { theme } = useTheme();
 
+  const {
+    isClosing,
+    isMounted,
+    close,
+  } = useModal({
+    isOpen,
+    onClose,
+    animationDelay: 300,
+  });
+
   const mods: Mods = {
     [cls.opened]: isOpen,
+    [cls.isClosing]: isClosing,
   };
+
+  if (lazy && !isMounted) {
+    return null;
+  }
 
   return (
     <Portal>
       <div className={classNames(cls.Drawer, mods, [className, theme, 'app_drawer'])}>
-        <Overlay onClick={onClose} />
-        <div
-          className={cls.content}
-        >
+        <Overlay onClick={close} />
+        <div className={cls.content}>
           {children}
         </div>
       </div>
