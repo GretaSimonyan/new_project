2f6770d327822b3eb4b1e388312846eeaadf5db1
GretaSimonyan <gsimonyan228@gmail.com>
2023-08-12 21:21:09 +0400

    97 Project documentation

--- src/shared/lib/hooks/useModal/useModal.tsx
+++ src/shared/lib/hooks/useModal/useModal.tsx
@@ -11,6 +11,14 @@ interface useModalProps {
   isOpen?: boolean;
   animationDelay: number;
 }
+
+/**
+ * Переиспользуемый хук для модальных компонентов (drawer/modal)
+ * @param animationDelay
+ * @param isOpen
+ * @param onClose
+ */
+
 export function useModal(props: useModalProps) {
   const {
     onClose,
